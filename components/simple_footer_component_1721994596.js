f31ba09b004bbac156c5a68ebb33ef43d970d651
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-26 11:50:05 +0000

    Placeholder text replacement

--- components/simple_footer_component_1721994596.js
+++ components/simple_footer_component_1721994596.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1721994596", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            Painter's Palette
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Unleash your inner artist with Painter's Palette, the ultimate platform for creating custom paints tailored to your unique vision. Explore a world of vibrant colors and textures, and let your creativity flow onto the canvas.
                     </div>
                 </div>
             </div>
